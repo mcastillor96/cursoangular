a33bcc5fe99db01a4345d528680a80ad4feec8f6
Miguel <mcastillo@jig.es>
2020-04-07 21:40:27 +0200

    Añadido comportamiento responsive cabecera con propiedades
    Eliminada la carpeta docs

--- src/app/cabecera/cabecera.component.ts
+++ src/app/cabecera/cabecera.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LibroclickedService } from '../libroclicked.service';
+import { TouchSequence } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-cabecera',
@@ -8,10 +9,26 @@ import { LibroclickedService } from '../libroclicked.service';
 })
 export class CabeceraComponent implements OnInit {
 
+  showCollapse: boolean;
+  collapse: boolean;
+
   // añadimos en la cabecera el servicio
-  constructor(public librosVistos: LibroclickedService) { }
+  constructor(public librosVistos: LibroclickedService) {
+    // this.showCollapse = false;
+    // this.collapse = false;
+   }
 
   ngOnInit(): void {
+  }
+
+  isCollapse() {
+    if(this.collapse == true) {
+      this.collapse = false;
+      return false;
+    } else {
+      this.collapse = true;
+      return true;
+    }
   }
 
 }
